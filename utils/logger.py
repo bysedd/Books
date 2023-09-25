4f0b64e224b7735639a2813e2554493596a46ff0
Felippe Andrade <4drade@gmail.com>
2023-09-25 20:31:52 -0300

    docs: update docstrings and logging in `func_log` decorator

--- utils/logger.py
+++ utils/logger.py
@@ -13,8 +13,8 @@ logging.basicConfig(
 
 def func_log(func):
     """
-    :param func: A function object that needs to be logged.
-    :return: A decorated function that logs the execution time of the provided function.
+    :param func: A function object that needs to log.
+    :return: A decorated function that logs the executing time of the provided function.
     """
 
     def wrapper(*args, **kwargs):
@@ -22,7 +22,7 @@ def func_log(func):
         result = func(*args, **kwargs)
         end = datetime.now()
         time_lapse = round((end - start).total_seconds(), 1)
-        logging.info(f"Executed '{func.__name__}' in {time_lapse}s")
+        log(f"Executed '{func.__name__}' in {time_lapse}s")
         return result
 
     return wrapper
@@ -42,5 +42,6 @@ if __name__ == '__main__':
         import time
         from random import randint
         time.sleep(randint(1, 3))
+
 
     example_func()
