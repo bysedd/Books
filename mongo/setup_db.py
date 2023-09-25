c6a8e0e20fca09314d0ad0079e2d653367d7d7ef
Felippe Andrade <4drade@gmail.com>
2023-09-25 20:34:33 -0300

    chore: update MongoDB connection URI and add the main capability

--- mongo/setup_db.py
+++ mongo/setup_db.py
@@ -6,9 +6,16 @@ def setup() -> MongoClient:
     Use this file to set up the database.
     :return:
     """
-    uri = "mongodb+srv://4drade:dede9895@cluster0.dwjmome.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://4drade:K690ttzVShJzUe57@cluster0.dwjmome.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a new client and connect to the server
     client = MongoClient(uri)
 
     return client.books
+
+
+if __name__ == '__main__':
+    # test connection
+    db = setup()
+    print(db.list_collection_names())
+    print(db.books_collection.find_one())
