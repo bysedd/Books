4d81463a9925b87a07f4e2c5ae42a8bf72ef33f0
Felippe Andrade <4drade@gmail.com>
2023-09-25 20:44:08 -0300

    ci: add security measures to MongoDB connection setup

--- mongo/setup_db.py
+++ mongo/setup_db.py
@@ -1,4 +1,9 @@
+import os
+
+from dotenv import load_dotenv
 from pymongo import MongoClient
+
+load_dotenv()
 
 
 def setup() -> MongoClient:
@@ -6,7 +11,8 @@ def setup() -> MongoClient:
     Use this file to set up the database.
     :return:
     """
-    uri = "mongodb+srv://4drade:K690ttzVShJzUe57@cluster0.dwjmome.mongodb.net/?retryWrites=true&w=majority"
+    user, password = os.getenv("MONGO_USER"), os.getenv("MONGO_PASSWORD")
+    uri = f"mongodb+srv://{user}:{password}@cluster0.dwjmome.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a new client and connect to the server
     client = MongoClient(uri)
